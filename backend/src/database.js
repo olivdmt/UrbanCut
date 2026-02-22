4fc37df255c284ac3a57b3f01c7ed99f1a3e4687
Mateus Dayson <atomic4fun@gmail.com>
2026-02-22 14:58:09 -0300

    Config database

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,12 +1,14 @@
-const knex = require("knex");
-require('dotenv').config();
+// backend/src/database.js
+const knexLib = require("knex");
+require("dotenv").config();
 
-const knex = require('knex')({
-    client: 'pg',
-    connection:
-        process.env.DATABASE_URL, //Render fornece essa env
-        pool: { min: 0, max: 10},
-        ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false} : false,
+const db = knexLib({
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  pool: { min: 0, max: 10 },
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
-module.exports = knex;
+module.exports = db;
