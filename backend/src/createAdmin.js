d2dca7a41c200e9f802f754ce4ba3f866e95d65d
Mateus Dayson <atomic4fun@gmail.com>
2026-02-28 18:58:06 -0300

    Updating...

--- backend/src/createAdmin.js
+++ backend/src/createAdmin.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 async function main() {
